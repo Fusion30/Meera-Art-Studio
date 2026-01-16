8a4d21f4a2f5a01659cee29f71a4058959ad9405
Fusion30 <amiyatyagisma@gmail.com>
2026-01-16 20:54:12 +0530

    Basic changes
    Added a second swiper.

--- Public/handleSwiper.js
+++ Public/handleSwiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper-1', {
     slidesPerView: 1,
     speed: 1500,
     effect: "fade",
@@ -15,5 +15,25 @@ const swiper = new Swiper('.swiper', {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
-    pagination: true,
+    pagination: {
+        el: ".swiper-pagination-1",
+    },
+});
+
+const swiper2 = new Swiper('.swiper-2', {
+    slidesPerView: 2,
+    speed:200,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true
+    },
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-2 .swiper-button-next",
+        prevEl: ".swiper-2 .swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination-2",
+    },
 });
